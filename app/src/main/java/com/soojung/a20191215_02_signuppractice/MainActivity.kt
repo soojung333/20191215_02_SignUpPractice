4bd18a553809645af834ad33909c73623efed0ac
soojung <blessme333@gmail.com>
2019-12-15 11:10:31 +0900

    Initial commit

---
+++ app/src/main/java/com/soojung/a20191215_02_signuppractice/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.soojung.a20191215_02_signuppractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

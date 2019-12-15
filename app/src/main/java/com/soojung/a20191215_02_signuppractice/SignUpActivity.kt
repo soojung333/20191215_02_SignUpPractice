093a722dadd7d619fcc5d67680816c13f0fd9b14
soojung <blessme333@gmail.com>
2019-12-15 11:39:19 +0900

    회원가입 화면 연습

---
+++ app/src/main/java/com/soojung/a20191215_02_signuppractice/SignUpActivity.kt
@@ -0,0 +1,25 @@
+package com.soojung.a20191215_02_signuppractice
+
+import android.os.Bundle
+
+// MainActivity를 SignUpActivity로 변경하고 싶다. => 커서올리고 Shift + F6
+class SignUpActivity : BaseActivity() {
+
+
+
+
+    override fun setupEvents() {
+
+
+    }
+
+    override fun setValues() {
+
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}

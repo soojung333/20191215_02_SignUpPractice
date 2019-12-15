9d1164eb52cadd056a0f7c9920cac02834cb46b0
soojung <blessme333@gmail.com>
2019-12-15 13:48:35 +0900

    캘린더 변수에 년/월/일 세팅  - 항목별로 각각 집어넣기 set(Calendar. YEAR, year ... )  - 년/월/일 한번에 집어넣기 set(year, month, dayOfMonth)

--- app/src/main/java/com/soojung/a20191215_02_signuppractice/SignUpActivity.kt
+++ app/src/main/java/com/soojung/a20191215_02_signuppractice/SignUpActivity.kt
@@ -14,6 +14,8 @@ import java.util.*
 // MainActivity를 SignUpActivity로 변경하고 싶다. => 커서올리고 Shift + F6
 class SignUpActivity : BaseActivity() {
 
+    var selectedBirthDay:Calendar? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -31,11 +33,29 @@ class SignUpActivity : BaseActivity() {
                     val selectedDateStr = "${year} / ${month} / ${dayOfMonth}"
                     birthDayTxt.text = selectedDateStr
 
+                    selectedBirthDay?.let {
+                        Log.d("생년월일선택","이미 선택된 값을 수정 - 다시선택 ")
+
+                    }.let {
+                        Log.d("생년월일선택","선택된 값이 새로 생김 - 처음 선택")
+                        selectedBirthDay = Calendar.getInstance()
+//                        Q. selectedBirthDay 에 담긴 날짜는 몇월 몇일 ? 2019년 12월 15일
+//                        이 담긴 날짜를 => 선택한 년/월/일로 대입.
+                    }
+
+                    selectedBirthDay?.set(Calendar.YEAR, year)
+                    selectedBirthDay?.set(Calendar.MONTH, month)
+                    selectedBirthDay?.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+                    selectedBirthDay?.set(year, month, dayOfMonth)
+
+
+
+
+                }, 2019, Calendar.DECEMBER, 15)
 
 //                    자바에서는 월을 0~11월로 사용함. 생각하는 것 보다 1 작은 숫자를 월로 넣어줘야 함.
 //                    Calendar 클래스의 변수를 활용해서 월을 입력하면 보기 직관적.
-
-                }, 2019, Calendar.DECEMBER, 15)
 
                 datePickerDialog.show()
 
